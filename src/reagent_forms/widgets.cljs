0c20e54871c42e9afcaf5dde32abb22a5734b697
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-08-30 19:21:07 -0400

    Update widgets.cljs

--- src/reagent_forms/widgets.cljs
+++ src/reagent_forms/widgets.cljs
@@ -7,8 +7,11 @@
 (defn get-value [doc id]
   (get @doc id))
 
-(defn set-value! [doc id value]
-  (swap! doc assoc id value))
+(defn mk-save-fn [listeners]
+  (fn [doc id value]
+    (swap! doc assoc id value)
+    (doseq [listener (id listeners)]
+      (listener doc id value))))
 
 ;;coerce the input to the appropriate type
 (defmulti format-type
@@ -23,95 +26,64 @@
 
 ;;bind the widget to the document based on its type
 (defmulti bind
-  (fn [widget _ _]
-    widget))
+  (fn [{:keys [widget]} _]
+    (if (some #{widget} [:text :numeric :password :email :textarea])
+      :input-widget widget)))
 
-(defn bind-input [widget id doc]
+(defmethod bind :input-widget
+  [{:keys [widget id]} {:keys [doc save!]}]
   {:value (get-value doc id)
-   :on-change #(set-value! doc id (->> % (value-of) (format-type widget)))})
+   :on-change #(save! doc id (->> % (value-of) (format-type widget)))})
 
-(defmethod bind :text [widget id doc]
-  (bind-input widget id doc))
-
-(defmethod bind :numeric [widget id doc]
-  (bind-input widget id doc))
-
-(defmethod bind :password [widget id doc]
-  (bind-input widget id doc))
-
-(defmethod bind :email [widget id doc]
-  (bind-input widget id doc))
-
-(defmethod bind :textarea [widget id doc]
-  (bind-input widget id doc))
-
-(defmethod bind :checkbox [widget id doc]
+(defmethod bind :checkbox
+  [{:keys [id]} {:keys [doc save!]}]
   {:value (get-value doc id)
-   :on-change #(set-value! doc id (.-checked (.-target %)))})
+   :on-change #(save! doc id (.-checked (.-target %)))})
 
 (defmethod bind :default [_ _ _])
 
-(defn set-opts
-  [[type {:keys [id widget] :as opts} & body] doc schema & [default-opts]]  
-  (into
-   [type
-    (merge default-opts (id schema) (bind widget id doc) opts)]
-   body))
+(defn set-attrs
+  [[type attrs & body] opts & [default-attrs]]
+  (into [type (merge default-attrs (bind attrs opts) attrs)] body))
 
 ;;initialize the widget by binding it to the document and setting default options
 (defmulti init-widget
-  (fn [[_ {:keys [widget]}] _ _]
-    (keyword widget)))
+  (fn [[_ {:keys [widget]}] _]
+    (let [widget (keyword widget)]
+      (if (some #{widget} [:text :password :email :textarea :checkbox])
+        :input-widget widget))))
 
-(defmethod init-widget :text
-  [widget doc schema]
-  (set-opts widget doc schema {:type :text :class "form-control"}))
+(defmethod init-widget :input-widget
+  [[_ {:keys [widget]} :as component] opts]
+  (set-attrs component opts {:type widget :class "form-control"}))
 
 (defmethod init-widget :numeric
-  [widget doc schema]
-  (set-opts widget doc schema {:type :text :class "form-control"}))
-
-(defmethod init-widget :password
-  [widget doc schema]
-  (set-opts widget doc schema {:type :password :class "form-control"}))
-
-(defmethod init-widget :email
-  [widget doc schema]
-  (set-opts widget doc schema {:type :email :class "form-control"}))
-
-(defmethod init-widget :textarea
-  [widget doc schema]
-  (set-opts widget doc schema {:class "form-control"}))
-
-(defmethod init-widget :checkbox
-  [widget doc schema]
-  (set-opts widget doc schema {:type :checkbox :class "form-control"}))
+  [component opts]
+  (set-attrs component opts {:type :text :class "form-control"}))
 
 (defmethod init-widget :radio
-  [[type {:keys [id widget value] :as opts} & body] doc schema]    
+  [[type {:keys [id widget value] :as attrs} & body] {:keys [doc save!]}]    
   (into
     [type
-     (merge (id schema)
-            {:type :radio
+     (merge {:type :radio
              :class "form-control"
-             :on-change #(set-value! doc id value)}
-            opts)]
+             :on-change #(save! doc id value)}
+            attrs)]
      body))
-  
 
-(defn- group-item [selections doc widget id multi? [type {:keys [key] :as opts} & body]]
+(defn- group-item [[type {:keys [key] :as attrs} & body] {:keys [doc save! multi-select]} selections widget id]
   (letfn [(handle-click! []
-           (if multi?
+           (if multi-select
              (do
                (swap! selections update-in [key] not)
-               (set-value! doc id (->> @selections (filter second) (map first))))
+               (save! doc id (->> @selections (filter second) (map first))))
              (do
                (reset! selections {key (not (key @selections))})                              
-               (set-value! doc id (when (key @selections) key)))))] 
+               (save! doc id (when (key @selections) key)))))] 
     
     (fn []               
       [type (merge {:class (if (key @selections) "active")
-                    :on-click handle-click!} opts) body])))
+                    :on-click handle-click!} attrs) body])))
 
 (defn- mk-selections [selectors doc-values]
   (->> selectors
@@ -119,38 +91,39 @@
        (into {}) atom))
 
 (defn selection-group
-  [[type {:keys [widget id] :as opts} & selection-items] doc schema multi?]
-  (let [selections (mk-selections selection-items (get-value doc id))
-        selectors (map (fn [item] [(group-item selections doc widget id multi? item)])
+  [[type {:keys [widget id] :as attrs} & selection-items] opts]
+  (let [selections (mk-selections selection-items (get-value (:doc opts) id))
+        selectors (map (fn [item] [(group-item item opts selections widget id)])
                        selection-items)]
-    (into [type opts] selectors)))
+    (into [type attrs] selectors)))
 
 (defmethod init-widget :single-select
-  [widget doc schema]
-  (selection-group widget doc schema false))
+  [widget opts]
+  (selection-group widget opts))
 
 (defmethod init-widget :multi-select
-  [widget doc schema]
-  (selection-group widget doc schema true))
+  [widget opts]
+  (selection-group widget (assoc opts :multi-select true)))
 
 (defmethod init-widget :list
-  [[type {:keys [widget id] :as opts} & options] doc schema]
+  [[type {:keys [widget id] :as attrs} & options] {:keys [doc save!]}]
   (let [selection (atom (or
                          (get-value doc id)
                          (get-in (first options) [1 :key])))]    
     (fn []      
-      [type (merge opts {:on-change #(set-value! doc id (value-of %))}) options])))
+      [type (merge attrs {:on-change #(save! doc id (value-of %))}) options])))
 
 (defn widget? [node]
   (and (coll? node)
        (map? (second node))
        (contains? (second node) :widget)))
 
-(defn bind-widgets [form schema doc]  
-  (let [form (clojure.walk/prewalk
+(defn bind-widgets [form doc & [listeners]]  
+  (let [opts {:doc doc :save! (mk-save-fn listeners)}
+        form (clojure.walk/prewalk
                (fn [node]
                  (if (widget? node)
-                   (let [widget (init-widget node doc schema)]
+                   (let [widget (init-widget node opts)]
                      (if (fn? widget) [widget] widget))
                    node))
                form)]    
