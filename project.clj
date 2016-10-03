4344ff78192dd2fc49c7f2a4310162ec02e0b222
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-10-03 19:30:50 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject reagent-forms "0.5.25"
+(defproject reagent-forms "0.5.26"
   :description "data binding library for Reagent"
   :url "https://github.com/yogthos/reagent-forms"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :clojurescript? true
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [reagent "0.5.0"]]
+  :dependencies [[reagent "0.6.0"]]
   :plugins [[codox "0.6.4"]]
   :profiles {:dev
              {:dependencies [[org.clojure/clojurescript "0.0-2322"]]
