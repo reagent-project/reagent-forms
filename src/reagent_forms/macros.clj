4554c4c9fdc2321ba25ad30348e7663543c57281
Oskar Zabik <oskar.zabik@gmail.com>
2017-11-25 01:47:58 +0100

    :disabled should support functions (#125)
    * Update render-element macro to accept a function under :disabled attr.
    
    * handle typeahead
    
    * Pass :disabled to render-element for :typeahead component
    
    * Also handle :disabled path for :timepicker
    
    * Actually disable datepicker input
    
    * don't try to update-in when path is not found

--- src/reagent_forms/macros.clj
+++ src/reagent_forms/macros.clj
@@ -2,7 +2,15 @@
 
 (defmacro render-element [attrs doc & body]
   `(fn []
-     (if-let [visible# (:visible? ~attrs)]
-       (when (visible# (deref ~doc))
-         ~@body)
-       ~@body)))
+     (let [disabled-path# (case (:field ~attrs)
+                           :typeahead [1 1 :disabled]
+                           :datepicker [1 1 1 :disabled]
+                           [1 :disabled])
+           body# (if (and (:disabled ~attrs)
+                          (get-in ~@body disabled-path#))
+                   (update-in ~@body disabled-path# #(if (fn? %) (%) %))
+                   ~@body)]
+       (if-let [visible# (:visible? ~attrs)]
+         (when (visible# (deref ~doc))
+           body#)
+         body#))))
