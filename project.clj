48804b466c133089a2ad3eb5c464983a41bb03a7
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-11-26 15:25:41 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-forms "0.5.31"
+(defproject reagent-forms "0.5.32"
   :description "data binding library for Reagent"
   :url "https://github.com/yogthos/reagent-forms"
   :license {:name "Eclipse Public License"
