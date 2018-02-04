c6322e8df431e5940223713622ce1a18027ad753
Oskar Zabik <oskar.zabik@gmail.com>
2018-02-04 19:14:40 +0100

    test core/mk-save-fn

--- test/reagent_forms/core_test.cljs
+++ test/reagent_forms/core_test.cljs
@@ -3,6 +3,10 @@
             [reagent.core :as r]
             [reagent-forms.core :as core]))
 
+(defn- update&double
+  [path value doc]
+  (update-in doc path * 2))
+
 (deftest scroll-to-test
   (let [f #'core/scroll-to
         item-elem {:scrollHeight 100
@@ -37,28 +41,33 @@
 (deftest run-events-test
   (let [state {}
         f1 (fn [path value doc]
-             (assoc-in doc path value))
-        f2 (fn [path value doc]
-             (update-in doc path * 2))
-        f3 (fn [path value doc]
-             (update-in doc path * 2))]
-    (is (= (core/run-events state :kw 2 [f1 f2 f3])
+             (assoc-in doc path value))]
+    (is (= (core/run-events state :kw 2 [f1 update&double update&double])
            {:kw 8}))))
 
 (deftest mk-update-fn-test
-  (testing "Value is associated in the doc"
+  (testing "Value is associated in the doc."
     (let [state (r/atom {:kw 5})
           f (core/mk-update-fn state [])
           update-fn (fn [_ val] val)]
-     (f :kw update-fn 10)
-     (is (= @state {:kw 10}))))
+     (f :kw update-fn :val)
+     (is (= @state {:kw :val}))))
   (testing "Returned function runs all the events."
     (let [state (r/atom {:kw 5})
-          f1 (fn [path value doc]
-               (update-in doc path * 2))
-          f2 (fn [path value doc]
-               (update-in doc path * 2))
-          f (core/mk-update-fn state [f1 f2])
+          f (core/mk-update-fn state [update&double update&double])
           update-fn (fn [_ val] val)]
      (f :kw update-fn 10)
      (is (= @state {:kw 40})))))
+
+(deftest mk-save-fn-test
+  (testing "Value is associated in the doc."
+    (let [state (r/atom {})
+          f (core/mk-save-fn state [])]
+     (f :kw :val)
+     (is (= @state
+            {:kw :val}))))
+  (testing "Returned function runs all the events."
+    (let [state (r/atom {})
+          f (core/mk-save-fn state [update&double update&double])]
+     (f :kw 1)
+     (is (= @state {:kw 4})))))
