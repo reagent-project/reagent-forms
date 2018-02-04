d2fb8d9662103441c2bf3fc6eb4bbe06c9ca83e7
Oskar Zabik <oskar.zabik@gmail.com>
2018-02-04 15:43:29 +0100

    test core/scroll-to

--- test/reagent_forms/core_test.cljs
+++ test/reagent_forms/core_test.cljs
@@ -1,3 +1,23 @@
 (ns reagent-forms.core-test
   (:require [clojure.test :refer [deftest is are]]
             [reagent-forms.core :as core]))
+
+(deftest scroll-to-test
+  (let [f #'core/scroll-to
+        item-elem {:scrollHeight 100
+                   :offsetTop 300}
+        list-elem (fn [i]
+                    (is (= i 0))
+                    (clj->js {:scrollTop 999
+                              :children
+                              {:item (fn [idx]
+                                       (is (= idx 3))
+                                       (clj->js item-elem))}}))
+        ul (fn [tag-name]
+             (is (= tag-name "ul"))
+             (clj->js {:item list-elem}))
+        element (clj->js
+                 {:target
+                  {:parentNode
+                   {:getElementsByTagName ul}}})]
+      (is (= 100 (core/scroll-to element 3)))))
