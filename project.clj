dfcd00c7d9871ca2b0e6bea325fd064f4dfe8cfd
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-04-27 09:32:46 -0400

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject reagent-forms "0.5.43"
+(defproject reagent-forms "0.5.44"
   :description "data binding library for Reagent"
   :url "https://github.com/yogthos/reagent-forms"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :clojurescript? true
-  :dependencies [[reagent "0.8.1"]]
+  :dependencies [[reagent "0.10.0"]]
   :plugins [[codox "0.10.4"]]
   :profiles {:dev
-             {:dependencies [[org.clojure/clojure "1.9.0"]
-                             [org.clojure/clojurescript "1.10.339"]
-                             [json-html "0.4.4"]
-                             [cider/piggieback "0.3.9"]
-                             [figwheel-sidecar "0.5.16"]
+             {:dependencies [[org.clojure/clojure "1.10.1"]
+                             [org.clojure/clojurescript "1.10.748"]
+                             [json-html "0.4.7"]
+                             [cider/piggieback "0.4.2"]
+                             [figwheel-sidecar "0.5.19"]
                              [doo "0.1.10" ]]
               :plugins      [[lein-cljsbuild "1.1.7"]
-                             [lein-figwheel "0.5.16"]
-                             [lein-doo "0.1.10"]]
+                             [lein-figwheel "0.5.19"]
+                             [lein-doo "0.1.11"]]
               :source-paths ["src" "dev"]
               :resource-paths ["dev"]
               :figwheel
